a32db14827f2dc6b8dd03c5f1a5fbf61a3244f85
Eleanor Cawthon <eleanor@eecs.berkeley.edu>
2017-02-15 16:00:18 -0800

    openmp: whitespace

--- openmp.cpp
+++ openmp.cpp
@@ -9,18 +9,18 @@
 //  benchmarking program
 //
 int main( int argc, char **argv )
-{   
-    int navg,nabsavg=0,numthreads; 
+{
+    int navg,nabsavg=0,numthreads;
     double dmin, absmin=1.0,davg,absavg=0.0;
-	
+
     if( find_option( argc, argv, "-h" ) >= 0 )
     {
         printf( "Options:\n" );
         printf( "-h to see this help\n" );
         printf( "-n <int> to set number of particles\n" );
         printf( "-o <filename> to specify the output file name\n" );
-        printf( "-s <filename> to specify a summary file name\n" ); 
-        printf( "-no turns off all correctness checks and particle output\n");   
+        printf( "-s <filename> to specify a summary file name\n" );
+        printf( "-no turns off all correctness checks and particle output\n");
         return 0;
     }
 
@@ -29,7 +29,7 @@ int main( int argc, char **argv )
     char *sumname = read_string( argc, argv, "-s", NULL );
 
     FILE *fsave = savename ? fopen( savename, "w" ) : NULL;
-    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;      
+    FILE *fsum = sumname ? fopen ( sumname, "a" ) : NULL;
 
     particle_t *particles = (particle_t*) malloc( n * sizeof(particle_t) );
     set_size( n );
@@ -40,7 +40,7 @@ int main( int argc, char **argv )
     //
     double simulation_time = read_timer( );
 
-    #pragma omp parallel private(dmin) 
+    #pragma omp parallel private(dmin)
     {
     numthreads = omp_get_num_threads();
     for( int step = 0; step < NSTEPS; step++ )
@@ -58,29 +58,29 @@ int main( int argc, char **argv )
             for (int j = 0; j < n; j++ )
                 apply_force( particles[i], particles[j],&dmin,&davg,&navg);
         }
-        
-		
+
+
         //
         //  move particles
         //
         #pragma omp for
-        for( int i = 0; i < n; i++ ) 
+        for( int i = 0; i < n; i++ )
             move( particles[i] );
-  
-        if( find_option( argc, argv, "-no" ) == -1 ) 
+
+        if( find_option( argc, argv, "-no" ) == -1 )
         {
           //
           //  compute statistical data
           //
           #pragma omp master
-          if (navg) { 
+          if (navg) {
             absavg += davg/navg;
             nabsavg++;
           }
 
           #pragma omp critical
-	  if (dmin < absmin) absmin = dmin; 
-		
+	  if (dmin < absmin) absmin = dmin;
+
           //
           //  save if necessary
           //
@@ -91,13 +91,13 @@ int main( int argc, char **argv )
     }
 }
     simulation_time = read_timer( ) - simulation_time;
-    
+
     printf( "n = %d,threads = %d, simulation time = %g seconds", n,numthreads, simulation_time);
 
     if( find_option( argc, argv, "-no" ) == -1 )
     {
       if (nabsavg) absavg /= nabsavg;
-    // 
+    //
     //  -The minimum distance absmin between 2 particles during the run of the simulation
     //  -A Correct simulation will have particles stay at greater than 0.4 (of cutoff) with typical values between .7-.8
     //  -A simulation where particles don't interact correctly will be less than 0.4 (of cutoff) with typical values between .01-.05
@@ -109,7 +109,7 @@ int main( int argc, char **argv )
     if (absavg < 0.8) printf ("\nThe average distance is below 0.8 meaning that most particles are not interacting");
     }
     printf("\n");
-    
+
     //
     // Printing summary data
     //
@@ -125,6 +125,6 @@ int main( int argc, char **argv )
     free( particles );
     if( fsave )
         fclose( fsave );
-    
+
     return 0;
 }
